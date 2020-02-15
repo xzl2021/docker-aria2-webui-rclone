3d077b2f65d389d6a82fe5ea8de7027369e6b00c
Lyraa <xzl2021@hotmail.com>
2020-02-16 00:08:16 +0800

    Add container name

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo docker run -d -p 6800:6800 -p 6880:80 -p 6888:8080 -v /home/aria2/config:/conf -v /home/aria2/data:/data -t aria2-webui-rclone:latest
+sudo docker run --name aria2 -d -p 6800:6800 -p 6880:80 -p 6888:8080 -v /home/aria2/config:/conf -v /home/aria2/data:/data -t aria2-webui-rclone:latest
 exit 0
