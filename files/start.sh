1e96a68069b9eaec7e9a5e5d08e886af7c65121e
xzl2021 <xzl2021@hotmail.com>
2020-02-15 16:53:22 +0800

    initial commit

---
+++ files/start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if [ ! -f /conf/aria2.conf ]; then
+	cp /files/aria2.conf /conf/aria2.conf
+	if [ $SECRET ]; then
+		echo "rpc-secret=${SECRET}" >> /conf/aria2.conf
+	fi
+fi
+if [ ! -f /conf/on-complete.sh ]; then
+	cp /files/on-complete.sh /conf/on-complete.sh && chmod +x /conf/on-complete.sh
+fi
+
+touch /conf/aria2.session
+
+darkhttpd /webui --port 80 &
+darkhttpd /data --port 8080 &
+aria2c --conf-path=/conf/aria2.conf
