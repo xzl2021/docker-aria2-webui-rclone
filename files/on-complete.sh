83228738566643b7b6f86e87119f507cb18dcb3e
xzl2021 <xzl2021@hotmail.com>
2020-02-16 20:56:38 +0800

    Enhance on-complete.sh

--- files/on-complete.sh
+++ files/on-complete.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-if [ $2 -eq 1 ]; then
-	mv "$3" /data
-fi
 echo [$(date)] $2, $3, $1 "<br>" >> /data/_log.html
+if [ $2 -eq 0 ]; then
+	exit 0
+fi
+filepath=$3
+mv -r "$3" "/data${filepath#/data}" || mv "$3" "/data${filepath#/data}"
+exit 0
